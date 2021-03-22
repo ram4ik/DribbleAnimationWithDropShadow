62ff7811c51c0ed4645aa6bc223707577d2d64b9
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-03-22 15:25:18 +0200

    Initial Commit

---
+++ DribbleAnimationWithDropShadow/DribbleAnimationWithDropShadowApp.swift
@@ -0,0 +1,17 @@
+//
+//  DribbleAnimationWithDropShadowApp.swift
+//  DribbleAnimationWithDropShadow
+//
+//  Created by Ramill Ibragimov on 22.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct DribbleAnimationWithDropShadowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
